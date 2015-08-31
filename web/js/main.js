6c644c56f14066f5bbf55d3ff03e22ef660ff697
jeisontorres <jeisonstg@gmail.com>
2015-08-31 11:13:14 -0500

    inicio poryecto en github

---
+++ web/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    $('#dropdown1-tab').click(function(){
+        $('#myTabDrop1').text('Día 1');
+    });
+    $('#dropdown2-tab').click(function(){
+        $('#myTabDrop1').text('Día 2');
+    });
+    $('#dropdown3-tab').click(function(){
+        $('#myTabDrop1').text('Día 1 y 2');
+    });
+
+});
